ca0168bde1a6abdfe5295c38d934ea92032ce963
meixshp <64128153+meixshp@users.noreply.github.com>
2020-06-19 17:30:55 +0200

    Update CSS

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -2,21 +2,21 @@
 var Aufgabe08;
 (function (Aufgabe08) {
     let submit = document.getElementById("button");
-    submit.addEventListener("click", communicate);
-    async function communicate() {
+    submit.addEventListener("click", hdlCommunicate);
+    async function hdlCommunicate() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://gis-example.herokuapp.com";
+        let url = "https://jiaies2020.herokuapp.com";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
         url += url + "?" + query.toString();
         let response = await fetch(url);
-        let requestURL = response.url;
+        let requestURL = await response.url;
         console.log(requestURL);
-        for (let entry of formData) {
+        /*for (let entry of formData) {
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
-        }
+        }*/
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -1,25 +1,25 @@
 namespace Aufgabe08 {
 
     let submit: HTMLElement = <HTMLElement> document.getElementById("button");
-    submit.addEventListener("click", communicate);
+    submit.addEventListener("click", hdlCommunicate);
 
-    async function communicate(): Promise<void> {
+    async function hdlCommunicate(): Promise<void> {
 
         let formData: FormData = new FormData(document.forms[0]);
 
-        let url: string = "https://gis-example.herokuapp.com";
+        let url: string = "https://jiaies2020.herokuapp.com";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
 
         let response: Response = await fetch(url);
-        let requestURL: string = response.url;
+        let requestURL: string = await response.url;
         console.log(requestURL);
         
-        for (let entry of formData) {
+        /*for (let entry of formData) {
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
-        }
+        }*/
     }
 }
