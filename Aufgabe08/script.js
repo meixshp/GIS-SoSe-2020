5c64bcfa48181f1949428304873cab42658d1088
meixshp <64128153+meixshp@users.noreply.github.com>
2020-06-21 23:38:52 +0200

    Update Script

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -3,15 +3,15 @@ var Aufgabe08;
 (function (Aufgabe08) {
     let submit = document.getElementById("button");
     submit.addEventListener("click", hdlCommunicate);
-    let formData = new FormData(document.forms[0]);
     async function hdlCommunicate() {
+        let formData = new FormData(document.forms[0]);
         let url = "https://jiaies2020.herokuapp.com";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
         url += url + "?" + query.toString();
         let response = await fetch(url);
-        let requestURL = await response.url;
-        console.log(requestURL);
+        //let requestURL = await response.url;
+        console.log(response.text);
         /*for (let entry of formData) {
             console.log(entry);
             console.log("name: " + entry[0]);
