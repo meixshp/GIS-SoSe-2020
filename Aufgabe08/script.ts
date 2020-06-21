0d7b0c4f76900d4ec89cf6440aa4c65547d3e03b
meixshp <64128153+meixshp@users.noreply.github.com>
2020-06-21 23:38:39 +0200

    Update Script

--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -3,18 +3,16 @@ namespace Aufgabe08 {
     let submit: HTMLButtonElement = <HTMLButtonElement> document.getElementById("button");
     submit.addEventListener("click", hdlCommunicate);
 
-    let formData: FormData = new FormData(document.forms[0]);
-
     async function hdlCommunicate(): Promise<void> {
-
+        let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://jiaies2020.herokuapp.com";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
 
         let response: Response = await fetch(url);
-        let requestURL: string = await response.url;
-        console.log(requestURL);
+        //let requestURL: string = await response.url;
+        console.log(response.text);
         
         /*for (let entry of formData) {
             console.log(entry);
